fcb12b1e460a48cf44a10c1db8b5817412287fa9
Mathis Cadio <cadio@et.esiea.fr>
2021-12-11 12:49:09 +0100

    Vérification position correcte lors requête attaque

--- src/main/java/fr/lernejo/navy_battle/api/game/fire/FireRequest.java
+++ src/main/java/fr/lernejo/navy_battle/api/game/fire/FireRequest.java
@@ -7,12 +7,35 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class FireRequest {
+    public final String[] alphabetCoo = new String[]{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
+
     public String getCooAttack(Scanner sc) {
-        System.out.println("Veuillez donner la colonne (lettre de A à J) et la rangée (de 1 à 10):");
-        return sc.nextLine();
+        while(true) {
+            System.out.println("Veuillez donner la colonne (lettre de A à J) et la rangée (de 1 à 10):");
+            String coo = sc.nextLine();
+            List<Integer> cell = new ArrayList<>();
+            if (coo.length() > 1 && coo.length() < 4) {
+                if (coo.length() == 2)
+                    cell.add(Integer.parseInt(coo.substring(1, 2)) - 1);
+                else
+                    cell.add(Integer.parseInt(coo.substring(1, 3)) - 1);
+                cell.add(List.of(this.alphabetCoo).indexOf(String.valueOf(coo.charAt(0))));
+                if (cell.get(0) < 0 || cell.get(1) < 0 || cell.get(0) > 9 || cell.get(1) > 9) { //Test si position est dans la grille
+                    System.out.println("Position incorrecte");
+                }
+                else {
+                    return coo;
+                }
+            }
+            else {
+                System.out.println("Position incorrecte");
+            }
+        }
     }
 
     public JSONObject fire(String adversaryUrl, String cell) {
