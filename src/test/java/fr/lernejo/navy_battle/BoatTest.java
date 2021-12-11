87a57d0f4d34cf075354e778ecbab06ce43ff622
Mathis Cadio <cadio@et.esiea.fr>
2021-12-11 01:56:16 +0100

    Mise en place code coverage avec junit-jupiter & assertj

---
+++ src/test/java/fr/lernejo/navy_battle/BoatTest.java
@@ -0,0 +1,16 @@
+package fr.lernejo.navy_battle;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BoatTest {
+
+    @Test
+    void boatPositions() {
+    }
+
+    @Test
+    void isBoatOnPosition() {
+    }
+}
