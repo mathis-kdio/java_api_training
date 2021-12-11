62b552200e5e5b9bf51d4dc8e5bbe415fec8cad1
Mathis Cadio <cadio@et.esiea.fr>
2021-12-11 02:23:15 +0100

    Ajout test pour boat.boatPositions()

--- src/test/java/fr/lernejo/navy_battle/BoatTest.java
+++ src/test/java/fr/lernejo/navy_battle/BoatTest.java
@@ -2,15 +2,22 @@ package fr.lernejo.navy_battle;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.assertj.core.api.Assertions;
 
 class BoatTest {
 
     @Test
-    void boatPositions() {
-    }
-
-    @Test
-    void isBoatOnPosition() {
+    void boatPositions_should_return_positions() {
+        ArrayList<List<Integer>> positions = new ArrayList<>();
+        List<Integer> position = new ArrayList<>();;
+        position.add(1);
+        position.add(1);
+        positions.add(position);
+        Boat boat = new Boat(Boat.BoatType.TORPILLEUR, positions);
+        Assertions.assertThat(boat.boatPositions()).as("return positions")
+            .isEqualTo(positions);
     }
 }
