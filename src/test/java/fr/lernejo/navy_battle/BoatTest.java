7c4b9040ee627f94aebbbaf0f40acb790f05e394
Mathis Cadio <cadio@et.esiea.fr>
2021-12-23 18:23:23 +0100

    Ajout test class Boat

--- src/test/java/fr/lernejo/navy_battle/BoatTest.java
+++ src/test/java/fr/lernejo/navy_battle/BoatTest.java
@@ -2,11 +2,13 @@ package fr.lernejo.navy_battle;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.assertj.core.api.Assertions;
 
 class BoatTest {
-
+    @Test
+    void boat_init_lastCoo() {
+        Boat boat = new Boat(Boat.BoatType.TORPILLEUR, new String[]{"J9", "J10"});
+        Assertions.assertThat(boat.positions).as("test last coo init boat")
+            .isEqualTo(new int[][]{{8,9},{9,9}});
+    }
 }
